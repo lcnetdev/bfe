7402804282557aff1c17671b2b6266ae0d4a9d34
Kevin Ford <kefo@3windmills.com>
2014-04-21 18:07:39 -0400

    Search entire list, return 20 results, on special character - a '*' in this case.

--- src/lookups/lclanguages.js
+++ src/lookups/lclanguages.js
@@ -20,7 +20,6 @@ define(function(require, exports, module) {
             clearTimeout(this.searching);
             process([]);
         }
-                
         this.searching = setTimeout(function() {
             if ( query.length > 2 ) {
                 u = "http://id.loc.gov/vocabulary/languages/suggest/?q=" + q;
@@ -29,6 +28,19 @@ define(function(require, exports, module) {
                     dataType: "jsonp",
                     success: function (data) {
                         parsedlist = processSuggestions(data, query);
+                        // save result to cache, remove next line if you don't want to use cache
+                        cache[q] = parsedlist;
+                        return process(parsedlist);
+                    }
+                });
+            } else if ( query.length === 1 && query == "*" ) {
+                u = "http://id.loc.gov/search/?format=jsonp&start=1&count=20&q=" + encodeURI("cs:http://id.loc.gov/vocabulary/languages");
+                console.log(u);
+                $.ajax({
+                    url: u,
+                    dataType: "jsonp",
+                    success: function (data) {
+                        parsedlist = processATOM(data, query);
                         // save result to cache, remove next line if you don't want to use cache
                         cache[q] = parsedlist;
                         return process(parsedlist);
@@ -86,6 +98,39 @@ define(function(require, exports, module) {
                 typeahead_source.push({ uri: u, value: l });
             }
         }
+        if (typeahead_source.length === 0) {
+            typeahead_source[0] = { uri: "", value: "[No suggestions found for " + query + ".]" };
+        }
+        //console.log(typeahead_source);
+        return typeahead_source;
+    }
+    
+    function processATOM(atomjson, query) {
+        var typeahead_source = [];
+        for (var k in atomjson) {
+            if (atomjson[k][0] == "atom:entry") {
+                var t = "";
+                var u = "";
+                var source = "";
+                for (var e in atomjson[k] ) {
+                    //alert(atomjson[k][e]);
+                    if (atomjson[k][e][0] == "atom:title") {
+                        //alert(atomjson[k][e][2]);
+                        t = atomjson[k][e][2];
+                    }
+                    if (atomjson[k][e][0] == "atom:link") {
+                        //alert(atomjson[k][e][2]);
+                        u = atomjson[k][e][1].href;
+                        source = u.substr(0, u.lastIndexOf('/'));
+                    }
+                    if ( t !== "" && u !== "") {
+                        typeahead_source.push({ uri: u, source: source, value: t });
+                        break;
+                    }
+                }
+            }
+        }
+        //alert(suggestions);
         if (typeahead_source.length === 0) {
             typeahead_source[0] = { uri: "", value: "[No suggestions found for " + query + ".]" };
         }
