f9cdd785e00cfe72f9a0682d07e6b6575fa2a33f
Kevin Ford <kefo@3windmills.com>
2014-04-22 18:01:59 -0400

    bfestore functions go here.  Need to add format support for exported data.

---
+++ src/bfestore.js
@@ -0,0 +1,62 @@
+define(function(require, exports, module) {
+    require("lib/lodash.min");
+
+    exports.store = [];
+    
+    exports.storeDedup = function() {
+        exports.store = _.uniq(exports.store, function(t) { 
+            if (t.olang !== undefined) {
+                return t.s + t.p + t.o + t.otype + t.olang
+            } else if (t.odatatype !== undefined) {
+                return t.s + t.p + t.o + t.otype + t.odatatype
+            } else {
+                return t.s + t.p + t.o + t.otype
+            }
+        });
+    }
+    
+    exports.jsonld2store = function(jsonld) {
+        jsonld.forEach(function(resource){
+            var s = typeof resource["@id"] !== 'undefined' ? resource["@id"] : '_:b' + guid();
+            for (var p in resource) {
+                if (p !== "@id") {
+                    resource[p].forEach(function(o) {
+                        var tguid = guid();
+                        var triple = {};
+                        triple.guid = tguid;
+                        triple.s = s;
+                        triple.p = p;
+                        if (o["@id"] !== undefined) {
+                            triple.o = o["@id"];
+                            triple.otype = "uri";
+                        } else if (o["@value"] !== undefined) {
+                            triple.o = o["@value"];
+                            triple.otype = "literal";
+                            if (o["@language"] !== undefined) {
+                                triple.olang = o["@language"];
+                            }
+                        }
+                        exports.store.push(triple);
+                        });
+                    }
+                }
+            // If a resource does not have a defined type, do we care?
+        });
+        return exports.store;
+    }
+    
+    /**
+    * Generates a GUID string.
+    * @returns {String} The generated GUID.
+    * @example af8a8416-6e18-a307-bd9c-f2c947bbb3aa
+    * @author Slavik Meltser (slavik@meltser.info).
+    * @link http://slavik.meltser.info/?p=142
+    */
+    function guid() {
+        function _p8(s) {
+            var p = (Math.random().toString(16)+"000000000").substr(2,8);
+            return s ? "-" + p.substr(0,4) + "-" + p.substr(4,4) : p ;
+        }
+        return _p8() + _p8(true) + _p8(true) + _p8();
+    }
+});
