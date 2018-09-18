90e196e49a1367b26f1ba1adfe0996837261dd66
Kirk Hess <kirk.hess@gmail.com>
2018-09-18 15:57:30 -0400

    more fixes for Work import

--- builds/bfe.dev.js
+++ builds/bfe.dev.js
@@ -755,21 +755,25 @@ bfe.define('src/bfe', ['require', 'exports', 'module', 'src/bfestore', 'src/bfel
 
     var $workmenudiv = $loadworkform.find('#bfeditor-loadwork-dropdownMenu');
 
-    for (var h = 0; h < config.startingPoints.length; h++) {
-          var sp = config.startingPoints[h];
-          var label = sp.menuGroup
-          for (var i = 0; i < sp.menuItems.length; i++) {
-            var $option = $('<option>', {
-              class: 'dropdown-item',
-              value: 'sp-' + h + '_' + i
-            });
-            if(sp.menuItems[i].type[0] === "http://id.loc.gov/ontologies/bibframe/Work" || sp.menuItems[i].type[0] === "http://id.loc.gov/ontologies/bibframe/Serial"){
-              //$a.html(sp.menuItems[i].label);
-              $option.html(label);
-              $workmenudiv.append($option);
-            }
-          }      
-        }
+    var getWorkProfileOptions = function(jqObject) {
+      for (var h = 0; h < config.startingPoints.length; h++) {
+        var sp = config.startingPoints[h];
+        var label = sp.menuGroup
+        for (var i = 0; i < sp.menuItems.length; i++) {
+          var $option = $('<option>', {
+            class: 'dropdown-item',
+            value: 'sp-' + h + '_' + i
+          });
+          if(sp.menuItems[i].type[0] === "http://id.loc.gov/ontologies/bibframe/Work"){
+            //$a.html(sp.menuItems[i].label);
+            $option.html(label);
+            jqObject.append($option);
+          }
+        }      
+      }
+    }
+
+    getWorkProfileOptions($workmenudiv);
 
     $loadworkdiv.append($loadworkform);
 
