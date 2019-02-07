4d4d3a307900888fc80769b9e877da7782d2c96a
Matt Miller <thisismattmiller@gmail.com>
2019-02-07 14:58:43 -0500

    Update config.js

--- static/js/config.js
+++ static/js/config.js
@@ -39,6 +39,8 @@ var config = {
               },*/ 
   "url" : rectoBase,
   "buildContext": true,
+  "buildContextFor": ['id.loc.gov/authorities/names/','id.loc.gov/authorities/subjects/','id.loc.gov/vocabulary/relators/','id.loc.gov/resources/works/'],
+  "buildContextForWorksEndpoint": 'mlvlp04.loc.gov:8230/resources/works/',
   "baseURI": "http://id.loc.gov/",
   "basedbURI": "http://mlvlp04.loc.gov:8230",
   "resourceURI": "http://mlvlp04.loc.gov:8230/resources",
