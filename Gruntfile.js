f355e918f8590e6f63bb074bf6f87b82380645ba
Kirk Hess <kirk.hess@gmail.com>
2019-04-03 09:25:33 -0400

    add test option in gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,6 +6,12 @@ module.exports = function(grunt) {
                 stripBanners: true,
                 banner: '/* <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */',
             },
+            test: {
+                files: {
+                'builds/bfe-test.js' : ['src/lib/mini_require.js', 'src/bfe.js','src/bfestore.js','src/bfelogging.js','src/bfeusertemplates.js','src/bfeliterallang.js','src/bfelookups.js', 'src/bfeapi.js','src/lib/aceconfig.js'],
+                'builds/bfe-test.css' : ['src/css/bootstrap.css', 'src/css/typeahead.css', 'src/css/bfeusertemplates.css', 'src/css/bfeliterallang.css'],
+                }
+            },
             dist: {
                 files: {
                 'builds/bfe.js' : ['src/lib/mini_require.js', 'src/bfe.js','src/bfestore.js','src/bfelogging.js','src/bfeusertemplates.js','src/bfeliterallang.js','src/bfelookups.js', 'src/bfeapi.js','src/lib/aceconfig.js'], 
@@ -52,5 +58,5 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-plato');
 
-	grunt.registerTask('default', ['concat','uglify', 'cssmin', 'plato']);
+	grunt.registerTask('default', ['concat:dist','uglify', 'cssmin', 'plato']);
 };
