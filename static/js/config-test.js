728d3b0f51c6659807d6d404fba8242136760ce0
Kevin Ford <kefo@loc.gov>
2020-10-15 16:21:31 -0400

    Update test and production configs.

--- static/js/config-test.js
+++ static/js/config-test.js
@@ -46,33 +46,26 @@ if (env.ENABLEUSERTEMPLATES!=undefined){
     enableusertemplates=env.ENABLEUSERTEMPLATES;
 }
 
-if (env.VERSOBASE!=undefined){
-    versobase=env.VERSOBASE;
-} else {
-    versobase=rectobase + "/verso";
-}
-
 var config = {
      "logging": {
       "level": "DEBUG",
       "toConsole": true
     }, 
-    "name":name,
-    "url": rectobase,
+    "name": name,
+    "url" : rectobase,
     "baseURI": "http://id.loc.gov/",
     "basedbURI": baseDBURI,
     "resourceURI": resourceURI,
     "metaproxyURI": metaproxyURI,
-    "versobase": versobase,
     "buildContext": buildcontext,
     "buildContextFor": ['id.loc.gov/authorities/names/','id.loc.gov/authorities/subjects/','http://id.loc.gov/authorities/childrensSubjects','id.loc.gov/vocabulary/relators/','id.loc.gov/resources/works/', 'id.loc.gov/bfentities/providers/','id.loc.gov/entities/providers/','id.loc.gov/authorities/genreForms'],
     "buildContextForWorksEndpoint": workContext,
     "enableUserTemplates" :enableusertemplates,
     "enableLoadMarc": loadmarc,
-    "startingPointsUrl": versobase + "/api/configs?filter[where][configType]=startingPoints&filter[where][name]=" + name,
-    "literalLangDataUrl": versobase + '/api/configs?filter[where][configType]=literalLangData',
+    "startingPointsUrl": "/api/listconfigs?where=index.resourceType:startingPoints&where=index.label:" + name,
+    "literalLangDataUrl": '/api/listconfigs?where=index.resourceType:literalLangData',
     "profiles": [
-        versobase + "/api/configs?filter[where][configType]=profile"
+        "/api/listconfigs?where=index.resourceType:profile"
     ],
     "api": ["save", "publish", "retrieveLDS", "retrieve", "deleteId", "setStartingPoints"],
     "return": {
--- static/js/config.js
+++ static/js/config.js
@@ -46,12 +46,6 @@ if (env.ENABLEUSERTEMPLATES!=undefined){
     enableusertemplates=env.ENABLEUSERTEMPLATES;
 }
 
-if (env.VERSOBASE!=undefined){
-    versobase=env.VERSOBASE;
-} else {
-    versobase=rectobase + "/verso";
-}
-
 var config = {
     "name": name,
     "url" : rectobase,
@@ -59,16 +53,15 @@ var config = {
     "basedbURI": baseDBURI,
     "resourceURI": resourceURI,
     "metaproxyURI": metaproxyURI,
-    "versobase": versobase,
     "buildContext": buildcontext,
     "buildContextFor": ['id.loc.gov/authorities/names/','id.loc.gov/authorities/subjects/','http://id.loc.gov/authorities/childrensSubjects','id.loc.gov/vocabulary/relators/','id.loc.gov/resources/works/', 'id.loc.gov/bfentities/providers/','id.loc.gov/entities/providers/','id.loc.gov/authorities/genreForms'],
     "buildContextForWorksEndpoint": workContext,
     "enableUserTemplates" :enableusertemplates,
     "enableLoadMarc": loadmarc,
-    "startingPointsUrl": versobase + "/api/configs?filter[where][configType]=startingPoints&filter[where][name]=" + name,
-    "literalLangDataUrl": versobase + '/api/configs?filter[where][configType]=literalLangData',
+    "startingPointsUrl": "/api/listconfigs?where=index.resourceType:startingPoints&where=index.label:" + name,
+    "literalLangDataUrl": '/api/listconfigs?where=index.resourceType:literalLangData',
     "profiles": [
-        versobase + "/api/configs?filter[where][configType]=profile"
+        "/api/listconfigs?where=index.resourceType:profile"
     ],
     "api": ["save", "publish", "retrieveLDS", "retrieve", "deleteId", "setStartingPoints"],
     "return": {
