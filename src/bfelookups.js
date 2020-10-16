811445fa319661359011846da89056e64880204d
Kevin Ford <kefo@loc.gov>
2020-10-16 17:46:23 -0400

    Quick, necessary fix for noteTypes, but needs to be tested.

--- src/bfelookups.js
+++ src/bfelookups.js
@@ -901,7 +901,7 @@ bfe.define('src/lookups/lcnames', ['require', 'exports', 'src/lookups/lcshared',
     var lcshared = require('src/lookups/lcshared');
     var bfelog = require('src/bfelogging');
     var cache = [];
-    exports.scheme = '/api/configs?filter[where][configType]=noteTypes&filter[fields][json]=true';
+    exports.scheme = '/ldp/verso/configs/?where=index.resourceType=noteTypes';
   
     exports.source = function (query, processSync, processAsync, formobject) {
       bfelog.addMsg(new Error(), 'INFO', query);
